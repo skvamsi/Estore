81991bb7c52556c1ac5298e1df37bb0d35a67d22
skvamsi <140873635+skvamsi@users.noreply.github.com>
2024-06-12 18:12:43 +0530

    Add files via upload

---
+++ gate7/scripts/products.js
@@ -0,0 +1,123 @@
+const mobiles = [
+    {
+        id : 1,
+        name : 'GATE 2024 ECE',
+        price : 200,
+        inStock : 5,
+        imgSrc : "/home/rgukt-ongole/Downloads/gate7/shopping-cart-main/images/gatebooks/gate2024.jpg"
+    },
+
+    {
+        id : 2,
+        name : "GATE 2020 MECHANICAL ENGINEERING",
+        price : 250,
+        inStock : 4,
+        imgSrc : "/home/rgukt-ongole/Downloads/gate7/shopping-cart-main/images/gatebooks/gate2020mech.jpg"
+    },
+
+    {
+        id : 3,
+        name :"GATE HANDBOOK COMPUTER SCIENCE",
+        price : 300,
+        inStock : 8,
+        imgSrc : "/home/rgukt-ongole/Downloads/gate7/shopping-cart-main/images/gatebooks/gatecse.jpg"
+    },
+
+    {
+        id : 4,
+        name : 'GATE-2018 ELECTRICAL ENGINEERING',
+        price : 350,
+        inStock : 8,
+        imgSrc : "/home/rgukt-ongole/Downloads/gate7/shopping-cart-main/images/gatebooks/gate.jpg"
+    },
+
+    {
+        id : 5,
+        name : 'GATE-2017 CE SOLVED PAPERS2000-2016',
+        price : 200,
+        inStock : 5,
+        imgSrc : "/home/rgukt-ongole/Downloads/gate7/shopping-cart-main/images/gatebooks/gate2018.jpg"
+    },
+
+    {
+        id : 6,
+        name : 'GATE 2024 CSE',
+        price : 250,
+        inStock : 10,
+        imgSrc : "/home/rgukt-ongole/Downloads/gate7/shopping-cart-main/images/gatebooks/GATE2024.jpg"
+    },
+
+    {
+        id : 7,
+        name :"GATE 2023 APTITUDE AND REASONING",
+        price : 250,
+        inStock : 12,
+        imgSrc : "/home/rgukt-ongole/Downloads/gate7/shopping-cart-main/images/gatebooks/general.png"
+    },
+
+    {
+        id : 8,
+        name : 'GATE GENERAL APTITUDE',
+        price : 300,
+        inStock : 4,
+        imgSrc : "/home/rgukt-ongole/Downloads/gate7/shopping-cart-main/images/gatebooks/gategeneral.jpg"
+    },
+
+    {
+        id : 9,
+        name : 'GATE-2023 CSE&IT SLOVED PAPERS',
+        price : 300,
+        inStock : 6,
+        imgSrc : "/home/rgukt-ongole/Downloads/gate7/shopping-cart-main/images/gatebooks/GATE.jpg"
+    }
+];
+const gameConsoles = [
+    {
+        id : 10,
+        name : 'GATE-BOOK',
+        price : 259,
+        inStock : 10,
+        imgSrc : "/home/rgukt-ongole/Downloads/gate7/shopping-cart-main/images/gatebooks/18.jpeg"
+    },
+
+    {
+        id : 11,
+        name : 'GATE-BOOK2',
+        price : 399,
+        inStock : 15,
+        imgSrc : "/home/rgukt-ongole/Downloads/gate7/shopping-cart-main/images/gatebooks/19.jpeg"
+    },
+
+    {
+        id : 12,
+        name : 'GATE-BOOK3',
+        price : 450,
+        inStock : 9,
+        imgSrc : "/home/rgukt-ongole/Downloads/gate7/shopping-cart-main/images/gatebooks/20.jpeg"
+    },
+
+    {
+        id : 13,
+        name : 'GATE-BOOK4',
+        price : 250,
+        inStock : 7,
+        imgSrc : "/home/rgukt-ongole/Downloads/gate7/shopping-cart-main/images/gatebooks/21.jpeg"
+    },
+
+    {
+        id : 14,
+        name : 'GATE-BOOK5',
+        price : 50,
+        inStock : 4,
+        imgSrc : "/home/rgukt-ongole/Downloads/gate7/shopping-cart-main/images/gatebooks/22.png"
+    },
+
+    {
+        id : 15,
+        name : 'GATE BOOK6',
+        price : 70,
+        inStock : 10,
+        imgSrc : "/home/rgukt-ongole/Downloads/gate7/shopping-cart-main/images/gatebooks/23.jpeg"
+    }
+];
+
